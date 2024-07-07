28d31e8e231aad037d88b6dd85bdae07d397b24e
anhongkui <anhongkui@jd.com>
2024-07-07 06:45:01 +0000

    fix: nodeSelector

--- middleware/install.sh
+++ middleware/install.sh
@@ -3,7 +3,11 @@
 helm list -n middleware | grep -w middleware
 if [ $? = 0 ]; then ACTION=upgrade; else ACTION=install; fi
 
+
 helm $ACTION middleware -n middleware ./ \
   --set global.storageClassName=csi-hostpath-fast \
   --set mysql.enabled=true \
-  --set redis.enabled=true
+  --set redis.enabled=true \
+  --set global.antiAffinity=false
+
+  # --set global.nodeSelector.dead=beef
