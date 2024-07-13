246514d1d119ab85afa67bb1d2c04eea9a56760e
anhongkui <anhongkui@jd.com>
2024-07-13 13:11:53 +0800

    etcd

--- middleware/install.sh
+++ middleware/install.sh
@@ -5,9 +5,15 @@ if [ $? = 0 ]; then ACTION=upgrade; else ACTION=install; fi
 
 
 helm $ACTION middleware -n middleware ./ \
-  --set global.storageClassName=csi-hostpath-fast \
-  --set mysql.enabled=true \
-  --set redis.enabled=true \
+  --set mysql.enabled=false \
+  --set mysql.storageClassName=csi-hostpath-fast \
+  --set mysql.storageSize=10Gi \
+  --set redis.enabled=false \
+  --set redis.storageClassName=csi-hostpath-fast \
+  --set redis.storageSize=2Gi \
+  --set etcd.enabled=true \
+  --set etcd.storageClassName=csi-hostpath-fast \
+  --set etcd.storageSize=4Gi \
   --set global.antiAffinity=false
 
   # --set global.nodeSelector.dead=beef
